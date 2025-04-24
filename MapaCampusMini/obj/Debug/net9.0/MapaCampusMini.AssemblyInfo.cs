e21d41613c92c8a48db60241812f23560a476dfc
Santiago <santiago.tucci@eldars.com.ar>
2025-04-24 18:01:35 -0300

    Instalación de React

--- MapaCampusMini/obj/Debug/net9.0/MapaCampusMini.AssemblyInfo.cs
+++ MapaCampusMini/obj/Debug/net9.0/MapaCampusMini.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MapaCampusMini")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a08ca0d2581c1419f035c7a2deedac876fb2e527")]
 [assembly: System.Reflection.AssemblyProductAttribute("MapaCampusMini")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MapaCampusMini")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
